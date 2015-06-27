1f9183de45d0f0eb7c4077179b11dee088e09e49
Nicolas Pauss <nicolas.pauss@gmail.com>
2015-06-28 01:16:48 +0200

    vim config

---
+++ conf/ycm_default_conf.py
@@ -0,0 +1,97 @@
+import os
+import ycm_core # pylint: disable=F0401
+
+def create_database(compilation_database_folder):
+    if not os.path.exists(compilation_database_folder):
+        return None
+    return ycm_core.CompilationDatabase(compilation_database_folder)
+
+
+def directory_of_this_script():
+    return os.path.dirname(os.path.abspath( __file__ ))
+
+
+def make_relative_paths_in_flags_absolute(flags, working_directory):
+    if not working_directory:
+        return list(flags)
+    new_flags = []
+    make_next_absolute = False
+    path_flags = [ '-isystem', '-I', '-iquote', '--sysroot=' ]
+    for flag in flags:
+        new_flag = flag
+
+        if make_next_absolute:
+            make_next_absolute = False
+            if not flag.startswith( '/' ):
+                new_flag = os.path.join( working_directory, flag )
+
+        for path_flag in path_flags:
+            if flag == path_flag:
+                make_next_absolute = True
+                break
+
+            if flag.startswith( path_flag ):
+                path = flag[ len( path_flag ): ]
+                new_flag = path_flag + os.path.join(working_directory, path)
+                break
+
+        if new_flag:
+            new_flags.append( new_flag )
+    return new_flags
+
+
+def is_header_file(filename):
+    extension = os.path.splitext(filename)[1]
+    return extension in ['.h', '.hxx', '.hpp', '.hh']
+
+
+SOURCE_EXTENSIONS = [ '.cpp', '.cxx', '.cc', '.c', '.m', '.mm', '.blk' ]
+
+
+class YcmDefaultConf(object):
+    def __init__(self, flags, compilation_database_folder):
+        self.flags = make_relative_paths_in_flags_absolute(
+            flags, directory_of_this_script())
+        self.database = create_database(compilation_database_folder)
+
+    def get_compilation_info_for_file(self, filename):
+        if not is_header_file(filename):
+            return self.database.GetCompilationInfoForFile(filename)
+
+        basename = os.path.splitext(filename)[0]
+        for extension in SOURCE_EXTENSIONS:
+            source_file = basename + extension
+            if not os.path.exists(source_file):
+                continue
+            compilation_info = self.database.GetCompilationInfoForFile(
+                source_file)
+            if compilation_info.compiler_flags_:
+                return compilation_info
+        return None
+
+    def make_flags_for_file(self):
+        def _with_database(filename):
+            compilation_info = self.get_compilation_info_for_file(filename)
+            if not compilation_info:
+                return None
+
+            return make_relative_paths_in_flags_absolute(
+                compilation_info.compiler_flags_,
+                compilation_info.compiler_working_dir_)
+
+        def _without_database(filename):
+            return self.flags
+
+        def _func(filename):
+            flags = _with_database(filename) if self.database else \
+                    _without_database(filename)
+
+            return {
+              'flags': flags,
+              'do_cache': True
+            }
+        return _func
+
+if __name__ != 'ycm_default_conf':
+    CONF = YcmDefaultConf(['-O0'], '')
+    FlagsForFile = CONF.make_flags_for_file() # pylint: disable=C0103
---
+++ dotfiles/vim/filetype.vim
@@ -0,0 +1,48 @@
+if exists("did_load_filetypes")
+    finish
+endif
+
+" C/C++
+au FileType c,cpp setlocal noignorecase textwidth=78
+au BufRead,BufNewFile *.blk,*.fc setf c
+au BufRead,BufNewFile *.blkk setf cpp
+
+let c_gnu=1
+let c_space_errors=1
+let c_no_curly_errors=1
+
+" IOP
+au FileType d setlocal noignorecase textwidth=78
+au BufRead,BufNewFile *.iop setf d
+
+" Javascript
+au FileType javascript setlocal cindent cinoptions-=L0.5s noignorecase textwidth=78 iskeyword+=$
+
+" PHP
+au FileType php setlocal et fo+=ro indentexpr= cin
+let php_noShortTags=1
+
+" Python
+au FileType python setlocal textwidth=78
+let python_highlight_all=1
+
+" Snippets
+au FileType snippets setlocal expandtab
+
+" Json
+au FileType json setlocal textwidth=78 fo=croql
+
+" Shell
+let g:is_bash=1
+
+" Diff
+au FileType diff setlocal nofoldenable
+
+" Asciidoc
+au FileType asciidoc setlocal spell spelllang=en_us
+au BufRead,BufNewFile *.adoc setf asciidoc
+
+" Git commit message
+let git_diff_spawn_mode=2
+au FileType gitcommit setlocal spell spelllang=en_us
+au BufNewFile,BufRead *.git/config,*/.git/config,.gitconfig setf dosini
