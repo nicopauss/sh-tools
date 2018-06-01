a7de8e6fb3c8d87d140c699d758fe5df919144da
Nicolas Pauss <nicolas.pauss@gmail.com>
2018-06-01 17:30:59 +0200

    Handle tmux > 2.2

--- dotfiles/tmux.sh
+++ dotfiles/tmux.sh
@@ -9,18 +9,20 @@ TMUX_VERSION="$(tmux -V | cut -c 6-)"
 load_conf() {
     local op="$1"
     local version="$2"
+    local file="$3"
 
     if [[ $(echo "$TMUX_VERSION $op $version" | bc) -eq 1 ]] ; then
         echo "Source files for $op $version"
-        tmux source-file "$TMUX_CONF_DIR/tmux-${version}.conf"
+        tmux source-file "$TMUX_CONF_DIR/$file"
     fi
 }
 
 main() {
     tmux source-file "$TMUX_CONF_DIR/tmux-global.conf"
-    load_conf ">" "2.0"
-    load_conf "<" "2.1"
-    load_conf "<" "2.2"
+    load_conf ">" "2.0" "tmux-2.0.conf"
+    load_conf "<" "2.1" "tmux-2.1.conf"
+    load_conf "<" "2.2" "tmux-2.2.conf"
+    load_conf ">" "2.2" "tmux-sup-2.2.conf"
     source "$TMUX_CONF_DIR/tmux-resurrect/resurrect.tmux"
 }
 
