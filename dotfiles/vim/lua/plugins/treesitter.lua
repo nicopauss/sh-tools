7e5cf7a770a00abefdc7edf78d8e70ca6d502a7e
Nicolas Pauss <nicolas.pauss@intersec.com>
2025-10-02 14:04:56 +0200

    [Vimrc] fix python indenting
    Treesitter is bad at indenting python so we need to run concurrently
    regex at the same time as treesitter, and disable TS (treesitter) indent

--- dotfiles/vim/lua/plugins/treesitter.lua
+++ dotfiles/vim/lua/plugins/treesitter.lua
@@ -18,14 +18,12 @@ require'nvim-treesitter.configs'.setup {
         end
     end,
 
-    -- Setting this to true will run `:h syntax` and tree-sitter at the same
-    -- time. Set this to `true` if you depend on 'syntax' being enabled (like
-    -- for indentation). Using this option may slow down your editor, and you
-    -- may see some duplicate highlights. Instead of true it can also be a list
-    -- of languages
-    additional_vim_regex_highlighting = false,
+    -- Slower (because we run regex + treesitter) but better for python indenting.
+    additional_vim_regex_highlighting = true,
   },
+
+  -- Bad at Python indenting
   indent = {
-    enable = true,
+    enable = false,
   },
 }
